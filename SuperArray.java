37b3464773008845037684a3555d0df1e830c0c6
Camille Sadoff <csadoff00@stuy.edu>
2018-10-23 11:51:46 -0400

    added constructor and intro methods

--- SuperArray.java
+++ SuperArray.java
@@ -1,3 +1,23 @@
 public class SuperArray{
-  
+  private String[] data;
+  private int size;
+  public SuperArray {
+    data = new String[10];
+  }
+  public void clear() {
+    size = 0;
+  }
+  public int size() {
+    return size;
+  }
+  public boolean isEmpty() {
+    return (size == 0);
+  }
+  public boolean add(String a) {
+    if (size < data.length) {
+      for (int i=0; i<data.length; i++) {
+
+      }
+    }
+  }
 }
