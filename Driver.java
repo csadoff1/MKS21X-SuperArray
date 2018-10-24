e1fcbd572e43ddfa40be698827314c398d871344
Camille Sadoff <csadoff00@stuy.edu>
2018-10-24 00:11:11 -0400

    began driver

---
+++ Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+  public static void main(String args[]) {
+    String[] a = new String[] {"hey ", "there ", "how ", "are ", "you", "?"};
+    SuperArray A = new SuperArray(a, 8);
+
+  }
+}
