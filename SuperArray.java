eb613519e81cb94b5a5599885336ed7fa6956ad0
Camille Sadoff <csadoff00@stuy.edu>
2018-10-25 06:46:58 -0400

    finished Phase 1

--- SuperArray.java
+++ SuperArray.java
@@ -1,30 +1,37 @@
 public class SuperArray{
   private String[] data;
   private int size;
-  public SuperArray(String[] data, int size) {
+
+  public SuperArray() {
     data = new String[10];
     this.size = size;
   }
+
   public void clear() {
     size = 0;
   }
+
   public int size() {
     return size;
   }
+
   public boolean isEmpty() {
     return (size == 0);
   }
+
   public boolean add(String a) {
     if (size < data.length) {
       for (int i=0; i<data.length; i++) {
-        if (data[i]== null) {
+        if (data[i] == null) {
           data[i] = a;
+          break;
         }
       }
       size++;
     }
     return true;
   }
+
   public String toString() {
     String newStr = "";
     newStr = newStr + "[";
@@ -38,6 +45,7 @@ public class SuperArray{
     }
     return newStr;
   }
+
   public String get(int i) {
     if (i < 0 || i >= size()) {
       return null;
@@ -46,4 +54,15 @@ public class SuperArray{
       return data[i];
     }
   }
+
+  public String set(int i, String str) {
+    if (i < 0 || i >= size()) {
+      return null;
+    }
+    else {
+      data[i] = str;
+    }
+    return data[i];
+  }
+
 }
